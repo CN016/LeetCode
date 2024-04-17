2759cb913b936156bd183eae1d7322547759c638
CN016 <meanful016@gmail.com>
2024-04-17 22:41:14 +0800

    端口扫描

--- src/com/my016/arithmetic/PortScanner.java
+++ src/com/my016/arithmetic/PortScanner.java
@@ -10,16 +10,17 @@ import java.util.Map;
 public class PortScanner {
     public static void main(String[] args) {
         String host = "106.52.247.57"; // 目标主机
-        int timeout = 2000; // 连接超时时间（毫秒）
+        int timeout = 600; // 连接超时时间（毫秒）
 
         System.out.println("开始端口扫描...");
         Map<Integer, Thread> threads = new HashMap<>(); // 线程列表，用于关闭线程
         for (int port = 1; port <= 65535; port++) {
             try {
-//                if (threads.size() >= 5 ){
+                if (threads.size() >= 5000 ){
 //                    port--;
-//                    continue;
-//                }
+                    Thread.sleep(10);
+                    continue;
+                }
                 int p = port;
                 Socket socket = new Socket();
                 Thread thread = new Thread(
